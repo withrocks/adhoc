15a82378e74ae23558db5da86e71d3027f12c0ca
Steinar Sturlaugsson <costeinar@gmail.com>
2025-12-28 03:12:19 +0100

    Remove temporary scripts

--- geocaching/music-quizz/parse_all_final_solutions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in htm-pages/*.htm; do
-    # Check if file exists (in case no .htm files are found)
-    if [ ! -f "$file" ]; then
-        continue
-    fi
-    python3 parse_html.py "$file" | tail -n 1
-done
